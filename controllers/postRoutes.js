87641702906990b1f1346ad70e79abd6fa7574a9
dimtaiwo <dimejitaiwo@Dimejis-MacBook-Pro.local>
2021-09-13 11:32:38 +0100

    create post working, validation working

--- controllers/postRoutes.js
+++ controllers/postRoutes.js
@@ -2,7 +2,8 @@ const express= require('express')
 const route = express.Router();
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid')
-const dayjs = require('dayjs')
+const dayjs = require('dayjs');
+const Joi = require('joi');
 
 
 route.get('/', (req, res)=>{
@@ -10,18 +11,53 @@ route.get('/', (req, res)=>{
 })
 
 route.post('/posts', (req, res)=>{
-    
-    const post ={
+
+    const schema = Joi.object({
+        text:Joi.string().min(2).max(200).required()
+    })
+       
+     const result = schema.validate(req.body)
+     if(result.error){
+         return res.status(400).send(result.error.details[0].message)
+     }
+
+     const postsObjects = {
+         posts:[]
+     }
+
+      const post ={
         id:uuidv4(),
         text:req.body.text,
         comments:[],
         likes:0,
         date:dayjs(Date.now()).format('DD-MM-YY HH:mm A')
     }
-    const postsData = fs.readFileSync('./model/posts.json', "utf-8");
-   res.send(post)
+
+    savePost(res, postsObjects,post)
+     
+    
+    
     
 })
+
+function savePost(res, postsObject, post){
+    const postsData = fs.readFileSync('./model/posts.json', "utf-8");
+    if(postsData.length === 0){
+        fs.appendFile(".model/posts.json", JSON.stringify(postsObject),(err)=>{
+            if(err) return res.status(500).send({"error":"Server error post not saved"})  
+            res.status(200).send(postsObject)
+        })
+    }else{
+        const parsedPostsData = JSON.parse(postsData)
+        parsedPostsData.posts.push(post)
+        fs.writeFile("./model/posts.json", JSON.stringify(parsedPostsData), (err)=>{
+            if(err) return res.status(500).send({"error":"Server error post not saved"}) 
+            res.status(200).send(parsedPostsData)
+        } )
+    }
+
+  
+}
 
 
 
