544209f27422701c35d6227c413b5a853acd00ea
dimtaiwo <dimejitaiwo@Dimejis-MacBook-Pro.local>
2021-09-16 13:46:18 +0100

    express installed

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const app = require('./server')
-const port = process.env.PORT || 3000
+const app = require("./server");
+const port = process.env.PORT || 3000;
 
-app.listen(port, ()=>{
-    console.log(`server starting on port ${port}`)
-})
-
+app.listen(port, () => {
+  console.log(`server starting on port ${port}`);
+});
--- server.js
+++ server.js
@@ -2,15 +2,8 @@ const express = require("express");
 const app = express();
 const postRoute = require("./controllers/postRoutes");
 const cors = require("cors");
-const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
 
 app.use("/", postRoute);
-
-app.listen(port, () => {
-  console.log(`server starting on port ${port}`);
-});
-
-module.exports = app;
