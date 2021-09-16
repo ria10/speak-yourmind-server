bd14b61e78d039d103b819c836c45fed7f0d482c
ria10 <ria1099@gmail.com>
2021-09-16 13:22:46 +0100

    edited tests to make them work

--- controllers/postRoutes.js
+++ controllers/postRoutes.js
@@ -1,210 +1,221 @@
-const express= require('express')
+const express = require("express");
 const route = express.Router();
-const fs = require('fs');
-const { v4: uuidv4 } = require('uuid')
-const dayjs = require('dayjs');
-const Joi = require('joi');
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+const dayjs = require("dayjs");
+const Joi = require("joi");
 
-
-
-route.get('/', (req, res)=>{
-    res.send("Hello world")
-})
+route.get("/", (req, res) => {
+  res.send("Hello world");
+});
 
 //get all posts
-route.get('/posts', (req, res)=>{
-    const postsData =  fs.readFileSync('./model/posts.json', 'utf-8')
-    if(postsData.length === 0) return res.send({"error":"no posts found"})
+route.get("/posts", (req, res) => {
+  const postsData = fs.readFileSync("./model/posts.json", "utf-8");
+  if (postsData.length === 0) return res.send({ error: "no posts found" });
 
-    const parsedPostsData = JSON.parse(postsData)
-    
+  const parsedPostsData = JSON.parse(postsData);
 
-    res.status(200).send(parsedPostsData.posts.reverse());
-})
+  res.status(200).send(parsedPostsData.posts.reverse());
+});
 
 //create a post
-route.post('/posts', (req, res)=>{
+route.post("/posts", (req, res) => {
+  const schema = Joi.object({
+    text: Joi.string().min(2).max(200).required(),
+  });
 
-    const schema = Joi.object({
-        text:Joi.string().min(2).max(200).required()
-    })
-       
-     const result = schema.validate(req.body)
-     if(result.error){
-         return res.send(result.error.details[0].message)
-     }
+  const result = schema.validate(req.body);
+  if (result.error) {
+    return res.send(result.error.details[0].message);
+  }
 
-     const postsObjects = {
-         posts:[]
-     }
+  const postsObjects = {
+    posts: [],
+  };
 
-      const post = {
-        id:uuidv4(),
-        text:req.body.text,
-        comments:[],
-        likes:[],
-        laughed:[],
-        crying:[],
-        date:dayjs(Date.now()).format('DD-MM-YY HH:mm A')
-    }
+  const post = {
+    id: uuidv4(),
+    text: req.body.text,
+    comments: [],
+    likes: [],
+    laughed: [],
+    crying: [],
+    date: dayjs(Date.now()).format("DD-MM-YY HH:mm A"),
+  };
 
-    savePost(res, postsObjects,post)
-      
-    
-})
+  savePost(res, postsObjects, post);
+});
 
 //get a single post by id
-route.get('/posts/:id', (req, res)=>{
-    const postsData =  fs.readFileSync('./model/posts.json', 'utf-8')
-    if(postsData.length === 0) return res.send(401).send({"error":"no posts found"})
-    const parsedPostsData = JSON.parse(postsData);
-    const requiredPost = parsedPostsData.posts.filter((post)=> post.id === req.params.id)
-    
-    if(requiredPost.length === 0) return  res.send(400).send({"error":"no post found"})
-     res.status(201).send(requiredPost[0]);  
-    
-})
+route.get("/post/:id", (req, res) => {
+  const postsData = fs.readFileSync("./model/posts.json", "utf-8");
+  if (postsData.length === 0)
+    return res.send(401).send({ error: "no posts found" });
+  const parsedPostsData = JSON.parse(postsData);
+  const requiredPost = parsedPostsData.posts.filter(
+    (post) => post.id === req.params.id
+  );
+
+  if (requiredPost.length === 0)
+    return res.send(400).send({ error: "no post found" });
+  res.status(201).send(requiredPost[0]);
+});
 //add a comment
-route.post('/post/:postId/comment', (req, res)=>{
-    
-    const schema = Joi.object({
-        text:Joi.string().min(2).max(200).required()
-    })
+route.post("/post/:postId/comment", (req, res) => {
+  const schema = Joi.object({
+    text: Joi.string().min(2).max(200).required(),
+  });
 
-    const result = schema.validate(req.body);
+  const result = schema.validate(req.body);
 
-    if(result.error) return res.send(result.error.details[0].message)
-    
-    const comment = {
-        text:req.body.text,
-        id:uuidv4(),
-        date:dayjs(Date.now()).format('DD-MM-YY HH:mm A')
+  if (result.error) return res.send(result.error.details[0].message);
+
+  const comment = {
+    text: req.body.text,
+    id: uuidv4(),
+    date: dayjs(Date.now()).format("DD-MM-YY HH:mm A"),
+  };
+  const postsContent = fs.readFileSync("./model/posts.json", "utf-8"); //postcontent
+
+  const parsedPostsContent = JSON.parse(postsContent);
+  const newArray = parsedPostsContent.posts.filter(
+    (post) => post.id === req.params.postId
+  );
+  const index = parsedPostsContent.posts.indexOf(newArray[0]);
+
+  parsedPostsContent.posts[index].comments.push(comment);
+
+  fs.writeFile(
+    "model/posts.json",
+    JSON.stringify(parsedPostsContent),
+    (err) => {
+      if (err) {
+        return res.send({ error: "server error" });
+      } else {
+        res.send(parsedPostsContent.posts[index].comments);
+      }
     }
-    const postsContent = fs.readFileSync("./model/posts.json", "utf-8"); //postcontent
-   
-    const parsedPostsContent = JSON.parse(postsContent);
-    const newArray = parsedPostsContent.posts.filter((post)=>post.id === req.params.postId);
-    const index = parsedPostsContent.posts.indexOf(newArray[0])
-    
-    parsedPostsContent.posts[index].comments.push(comment)
-
-    fs.writeFile("model/posts.json", (JSON.stringify(parsedPostsContent)),(err)=>{
-        if(err){
-           return res.send({"error":"server error"})
-        }else {
-            res.send(parsedPostsContent.posts[index].comments)
-        }
-    })
-   
-})
-
+  );
+});
 
 //get all comments
-route.get('/post/:postId/comment', (req, res)=>{
-    const Posts = fs.readFileSync('./model/posts.json', "utf-8")
+route.get("/post/:postId/comment", (req, res) => {
+  const Posts = fs.readFileSync("./model/posts.json", "utf-8");
 
-    const parsedPosts = JSON.parse(Posts)
+  const parsedPosts = JSON.parse(Posts);
 
-  const post =  parsedPosts.posts.find((post)=>{
-       return post.id === req.params.postId
-    })
+  const post = parsedPosts.posts.find((post) => {
+    return post.id === req.params.postId;
+  });
 
-    if(!post) return res.send({"error":"cant find a matching post"})
+  if (!post) return res.send({ error: "cant find a matching post" });
 
-    res.send(post.comments);
-})
+  res.send(post.comments);
+});
 
+route.post("/post/:postId/laugh", (req, res) => {
+  const laugh = {
+    text: "laughed",
+  };
+  const postsContent = fs.readFileSync("./model/posts.json", "utf-8"); //postcontent
 
+  const parsedPostsContent = JSON.parse(postsContent);
+  const newArray = parsedPostsContent.posts.filter(
+    (post) => post.id === req.params.postId
+  );
+  const index = parsedPostsContent.posts.indexOf(newArray[0]);
+  console.log(parsedPostsContent.posts[index]);
+  parsedPostsContent.posts[index].laughed.push(laugh);
 
-
-route.post('/post/:postId/laugh', (req, res)=>{
-    const laugh = {
-        text:"laughed",
+  fs.writeFile(
+    "model/posts.json",
+    JSON.stringify(parsedPostsContent),
+    (err) => {
+      if (err) {
+        return res.send({ error: "server error" });
+      } else {
+        res.send(parsedPostsContent.posts[index].laughed);
+      }
     }
-    const postsContent = fs.readFileSync("./model/posts.json", "utf-8"); //postcontent
-   
-    const parsedPostsContent = JSON.parse(postsContent);
-    const newArray = parsedPostsContent.posts.filter((post)=>post.id === req.params.postId);
-    const index = parsedPostsContent.posts.indexOf(newArray[0])
-     console.log(parsedPostsContent.posts[index])
-    parsedPostsContent.posts[index].laughed.push(laugh)
+  );
+});
 
+route.post("/post/:postId/crying", (req, res) => {
+  const cry = {
+    text: "cry",
+  };
+  const postsContent = fs.readFileSync("./model/posts.json", "utf-8"); //postcontent
 
-    fs.writeFile("model/posts.json", (JSON.stringify(parsedPostsContent)),(err)=>{
-        if(err){
-           return res.send({"error":"server error"})
-        }else {
-            res.send(parsedPostsContent.posts[index].laughed)
-        }
-    })
-})
-route.post('/post/:postId/crying', (req, res)=>{
-    const cry = {
-        text:"cry",
+  const parsedPostsContent = JSON.parse(postsContent);
+  const newArray = parsedPostsContent.posts.filter(
+    (post) => post.id === req.params.postId
+  );
+  const index = parsedPostsContent.posts.indexOf(newArray[0]);
+  console.log(parsedPostsContent.posts[index]);
+  parsedPostsContent.posts[index].crying.push(cry);
+
+  fs.writeFile(
+    "model/posts.json",
+    JSON.stringify(parsedPostsContent),
+    (err) => {
+      if (err) {
+        return res.send({ error: "server error" });
+      } else {
+        res.send(parsedPostsContent.posts[index].crying);
+      }
     }
-    const postsContent = fs.readFileSync("./model/posts.json", "utf-8"); //postcontent
-   
-    const parsedPostsContent = JSON.parse(postsContent);
-    const newArray = parsedPostsContent.posts.filter((post)=>post.id === req.params.postId);
-    const index = parsedPostsContent.posts.indexOf(newArray[0])
-     console.log(parsedPostsContent.posts[index])
-    parsedPostsContent.posts[index].crying.push(cry)
+  );
+});
 
+route.post("/post/:postId/likes", (req, res) => {
+  const like = {
+    text: "liked",
+  };
+  const postsContent = fs.readFileSync("./model/posts.json", "utf-8"); //postcontent
 
-    fs.writeFile("model/posts.json", (JSON.stringify(parsedPostsContent)),(err)=>{
-        if(err){
-           return res.send({"error":"server error"})
-        }else {
-            res.send(parsedPostsContent.posts[index].crying)
-        }
-    })
-})
+  const parsedPostsContent = JSON.parse(postsContent);
+  const newArray = parsedPostsContent.posts.filter(
+    (post) => post.id === req.params.postId
+  );
+  const index = parsedPostsContent.posts.indexOf(newArray[0]);
+  console.log(parsedPostsContent.posts[index]);
+  parsedPostsContent.posts[index].likes.push(like);
 
-route.post('/post/:postId/likes', (req, res)=>{
-    const like = {
-        text:"liked",
+  fs.writeFile(
+    "model/posts.json",
+    JSON.stringify(parsedPostsContent),
+    (err) => {
+      if (err) {
+        return res.send({ error: "server error" });
+      } else {
+        res.send(parsedPostsContent.posts[index].likes);
+      }
     }
-    const postsContent = fs.readFileSync("./model/posts.json", "utf-8"); //postcontent
-   
-    const parsedPostsContent = JSON.parse(postsContent);
-    const newArray = parsedPostsContent.posts.filter((post)=>post.id === req.params.postId);
-    const index = parsedPostsContent.posts.indexOf(newArray[0])
-     console.log(parsedPostsContent.posts[index])
-    parsedPostsContent.posts[index].likes.push(like)
+  );
+});
 
-
-    fs.writeFile("model/posts.json", (JSON.stringify(parsedPostsContent)),(err)=>{
-        if(err){
-           return res.send({"error":"server error"})
-        }else {
-            res.send(parsedPostsContent.posts[index].likes)
-        }
-    })
-})
-
-
-
-function savePost(res, postsObject, post){
-    const postsData = fs.readFileSync('./model/posts.json', "utf-8");
-    if(postsData.length === 0){
-        fs.appendFile(".model/posts.json", JSON.stringify(postsObject),(err)=>{
-            if(err) return res.send({"error":"Server error post not saved"})  
-            console.log("Hi 1")
-            res.status(201).send(post)
-        })
-    }else{
-        const parsedPostsData = JSON.parse(postsData)
-        parsedPostsData.posts.push(post)
-        fs.writeFile("./model/posts.json", JSON.stringify(parsedPostsData), (err)=>{
-            if(err) return res.send({"error":"Server error post not saved"}) 
-            console.log('Hi 2')
-            res.status(201).send(post)
-        } )
-    }
+function savePost(res, postsObject, post) {
+  const postsData = fs.readFileSync("./model/posts.json", "utf-8");
+  if (postsData.length === 0) {
+    fs.appendFile(".model/posts.json", JSON.stringify(postsObject), (err) => {
+      if (err) return res.send({ error: "Server error post not saved" });
+      console.log("Hi 1");
+      res.status(201).send(post);
+    });
+  } else {
+    const parsedPostsData = JSON.parse(postsData);
+    parsedPostsData.posts.push(post);
+    fs.writeFile(
+      "./model/posts.json",
+      JSON.stringify(parsedPostsData),
+      (err) => {
+        if (err) return res.send({ error: "Server error post not saved" });
+        console.log("Hi 2");
+        res.status(201).send(post);
+      }
+    );
+  }
 }
-
-
-
-
 
 module.exports = route;
---
+++ test/server.spec.js
@@ -0,0 +1,67 @@
+const request = require("supertest");
+const app = require("../server");
+const port = process.env.PORT || 5000;
+const postsData = require("../model/posts.json");
+
+describe("API server", () => {
+    let api;
+  
+    beforeAll(() => {
+      api = app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+      });
+    });
+  
+    afterAll((done) => {
+      console.log("Stopping the server.");
+      api.close(done);
+    });
+
+    test("should return /post/:id/comment with correct info and status code", (done) => {
+        request(api)
+          .get("/post/:id/comment")
+          .expect(200)
+          .then((req) => {
+            expect(req.body.comments);
+            done();
+          });
+      });
+      
+    test("should return /posts with correct info and status code", (done) => {
+        request(api)
+          .get("/posts")
+          .expect(200)
+          .then(() => {
+            expect(postsData.posts);
+            done();
+          });
+      });
+
+    test("404 for unknown paths", (done) => {
+        request(api).get("/hello").expect(404, done);
+      });
+
+    test("should add test post to the postsData", (done) => {
+        let testPost = { text: "Hello, this is a test post" };
+        request(api)
+          .post("/posts")
+          .send(testPost)
+          .expect(201)
+          .then((req) => {
+            expect(req.body.text).toEqual("Hello, this is a test post");
+            done();
+          })
+          
+      });
+
+    test("should return /posts/:id with correct info and status code", (done) => {
+        request(api)
+          .get("/post/7f4f4da3-00a7-409a-90e5-9654d1852c44")
+          .then((req) => {
+            expect(200);
+            expect(req.body.text).toEqual("first post");
+            done();
+          });
+          
+      });
+})
