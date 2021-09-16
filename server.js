4572e256a84fa64e6105335eb25c63cd456bc9f5
dimtaiwo <dimejitaiwo@Dimejis-MacBook-Pro.local>
2021-09-16 13:24:46 +0100

    chnaged to server

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const postRoute = require("./controllers/postRoutes")
-const cors = require('cors')
-const postsData = require('./model/posts.json')
+const postRoute = require("./controllers/postRoutes");
+const cors = require("cors");
+const port = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(cors());
 
-app.use(express.json())
-app.use(cors())
+app.use("/", postRoute);
 
-app.use('/', postRoute)
-
-
-
+app.listen(port, () => {
+  console.log(`server starting on port ${port}`);
+});
 
 module.exports = app;
